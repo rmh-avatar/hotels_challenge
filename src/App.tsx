2ae0deeb1875b76988616f3764858aa981b9357e
rmh-avatar <rmarrero9211@gmail.com>
2022-04-08 11:31:58 -0400

    add material date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DateAdapter from '@mui/lab/AdapterDateFns';
 import React from 'react';
+
 import './App.css';
 import SelectHotelPage from "./presentation/SelectHotelPage";
 
 function App() {
-  return (<SelectHotelPage/>
-  );
+    return (
+        <LocalizationProvider dateAdapter={DateAdapter}><SelectHotelPage/></LocalizationProvider>);
 }
 
 export default App;
